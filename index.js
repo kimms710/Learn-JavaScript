9044a8ae74ee41a8addba2b642ee8146f49dd1a4
kimms710 <kimms710@naver.com>
2026-06-30 10:37:20 +0900

    2026 멀캠 javascript 강의

---
+++ index.js
@@ -0,0 +1,37 @@
+
+console.log("멀캠 수업 중...");
+
+// 변수
+const userName = "성시기";
+console.log(userName);
+
+const userID = "ms"
+
+//mission
+//적절한 변수명에 사용자의 이름을 감아 변수 만들기
+//console에 출력해서 확인하기
+//const userMS; //const 변수값 변경 불가
+//userMS = "MinSung"; //오류 발생
+let userMS; // 변수값 변경 가능
+userMS = "MinSung"; //재 할당 가능
+console.log("user name is ", userMS);
+
+// Date Type
+const userAge = 30;
+const userMBTI = "ENFJ";
+console.log("user MBTI is ", userMBTI);
+
+//boolean type
+const isLoggedin = false; //boolean type으로 변수명을 만들때 암묵적으로 앞에 is 또는 has 를 붙혀서 만들어준다
+console.log("user isLoggedin: ", isLoggedin);
+console.log("isLoggedin Type ?", typeof isLoggedin);
+
+// null/undefined
+const empty1 = null;
+let empty2; //변수 선언만
+console.log("empty1 value ?", empty1);
+console.log("empty2 Type ?", typeof empty2);
+
+//mission 나머지 변수들도 타입확인하는 코드 작성 콘솔에 출력
+//console.log(" Type ?", typeof );
+
