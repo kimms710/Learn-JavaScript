80bdf9edef03399a4a172c1b4696f4606e0dc0e0
kimms710 <kimms710@naver.com>
2026-06-30 17:07:04 +0900

    반복문 - forEach

--- for.js
+++ for.js
@@ -1,4 +1,35 @@
-//for문
+// 반복문
+
+/*
+* 3가지 구성용소
+  1. 초기화 -> 1번 실행
+  2. 반복 조건식
+  3. 증감식
+*/
+
+// for
 for (let i = 0; i < 10; i++) {
-    console.log(i, '번째 실행');
+    // 반복 조건식이 true인 동안 실행할 반복 로직
+    // console.log(i, '번째 실행');
+    console.log(`${i}번째 실행`);
 }
+
+// mission 10 -> 1 count
+for (let i = 10; i > 0; i--) {
+    // 반복 조건식이 true인 동안 실행할 반복 로직
+    // console.log(i, '번째 실행');
+    console.log('COUNT DOWN: ', i);
+}
+
+const animals = ["rabbit", "puppy", "kitten", "panda", "fox"];
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(`🐾${animals[i]}🦊`);
+    animals[i]; // date type: string
+}
+
+// 배열 제공 함수 forEach (향상된 for문)
+animals.forEach((value, index) => {
+    // 각 요소들을 순회하면서 실행 반복 로직
+    console.log(`🐾${index}번째: ${value}🐶`)
+})
